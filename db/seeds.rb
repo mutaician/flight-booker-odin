b946e21f61cb30f1e2275f8fdc7a8f0cea8b44c5
mutaician <109066669+mutaician@users.noreply.github.com>
2024-04-22 22:43:38 +0300

    Set uo flight and airport associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Airport.create(code: 'JFK')
+Airport.create(code: 'LAX')
+Airport.create(code: 'ORD')
+Airport.create(code: 'DFW')
+Airport.create(code: 'ATL')
