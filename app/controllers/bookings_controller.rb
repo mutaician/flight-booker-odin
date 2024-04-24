2b311f609f219788395144d4fff036159faae49d
mutaician <109066669+mutaician@users.noreply.github.com>
2024-04-24 13:27:28 +0300

    basic functionality

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,27 @@
+class BookingsController < ApplicationController
+  def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = @flight.bookings.build
+    passengers = params[:passengers].to_i
+    passengers.times { @booking.passengers.build }
+  end
+
+  def create
+    @flight = Flight.find(params[:booking][:flight_id])
+    @booking = @flight.bookings.build(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
 
   root "flights#index"
   resources :flights, only: [:index]
+
+  resources :bookings, only: [:new, :create, :show]
 end
